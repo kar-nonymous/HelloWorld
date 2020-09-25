7b988d5920d0f683522bd6a2f3f223ce1d7f1d2a
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-09-25 11:02:57 +0530

    First push

---
+++ helloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloWorld
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple C# program
+            
+            Console.WriteLine("Hello World");
+        }
+    }
+}
